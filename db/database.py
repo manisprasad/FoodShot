3493fb94bba1f4c7d7fd5f05f1805a1b892fc0d4
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2026-07-08 16:16:31 +0300

    fix: explicitly disable SSL in database engine and alembic migrations configuration

--- db/database.py
+++ db/database.py
@@ -3,6 +3,11 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 from core.config import config
 
 engine = create_async_engine(
-    config.DATABASE_URL, echo=False, pool_size=20, max_overflow=10, pool_pre_ping=True
+    config.DATABASE_URL,
+    echo=False,
+    pool_size=20,
+    max_overflow=10,
+    pool_pre_ping=True,
+    connect_args={"ssl": False},
 )
 SessionLocal = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
